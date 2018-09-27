57a0eaf2af0977988d7480304fabe6fecd6d9645
webe0586 <webe0586@umn.edu>
2018-09-27 13:21:37 -0500

    Restore Dashboard..

---
+++ dashboard/README.md
@@ -0,0 +1,1 @@
+# teslaworks.demo
---
+++ dashboard/demo/src/views/UserProfile/UserProfile.jsx
@@ -0,0 +1,115 @@
+import React, { Component } from "react";
+import {
+  Grid,
+  Row,
+  Col,
+  FormGroup,
+  ControlLabel,
+  FormControl
+} from "react-bootstrap";
+
+import { Card } from "components/Card/Card.jsx";
+import { FormInputs } from "components/FormInputs/FormInputs.jsx";
+import { UserCard } from "components/UserCard/UserCard.jsx";
+import Button from "components/CustomButton/CustomButton.jsx";
+
+import avatar from "assets/img/faces/face-3.jpg";
+
+class UserProfile extends Component {
+  render() {
+    return (
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col md={8}>
+              <Card
+                title="Edit Profile"
+                content={
+                  <form>
+                    <FormInputs
+                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
+                      proprieties={[
+                        {
+                          label: "Officer Title",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "El Presidente",
+                          disabled: true
+                        },
+                        {
+                          label: "Username",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Username"
+                        },
+                        {
+                          label: "Email address",
+                          type: "email",
+                          bsClass: "form-control",
+                          placeholder: "Email"
+                        }
+                      ]}
+                    />
+                    <FormInputs
+                      ncols={["col-md-6", "col-md-6"]}
+                      proprieties={[
+                        {
+                          label: "First name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "First name",
+                        },
+                        {
+                          label: "Last name",
+                          type: "text",
+                          bsClass: "form-control",
+                          placeholder: "Last name",
+                        }
+                      ]}
+                    />
+                    <Button bsStyle="info" pullRight fill type="submit">
+                      Update Profile
+                    </Button>
+                    <div className="clearfix" />
+                  </form>
+                }
+              />
+            </Col>
+            <Col md={4}>
+              <UserCard
+                bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
+                avatar={avatar}
+                name="Mike Andrew"
+                userName="michael24"
+                description={
+                  <span>
+                    "Lamborghini Mercy
+                    <br />
+                    Your chick she so thirsty
+                    <br />
+                    I'm in that two seat Lambo"
+                  </span>
+                }
+                socials={
+                  <div>
+                    <Button simple>
+                      <i className="fa fa-facebook-square" />
+                    </Button>
+                    <Button simple>
+                      <i className="fa fa-twitter" />
+                    </Button>
+                    <Button simple>
+                      <i className="fa fa-google-plus-square" />
+                    </Button>
+                  </div>
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
